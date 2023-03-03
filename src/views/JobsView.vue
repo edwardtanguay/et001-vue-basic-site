944a80e74703ea12178abf17455bf7b813d5bb4a
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-03 16:05:29 +0100

    jobs in

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+import type { IJob } from '@/interfaces';
+import axios from 'axios';
+const jobs: IJob[] = (await axios.get('https://edwardtanguay.vercel.app/share/jobs.json')).data;
+</script>
+
 <template>
 	<div class="jobs">
-		<p>This is the jobs page.</p>
+		<p>There are {{ jobs.length }} jobs</p>
+		<ul>
+			<li v-for="job in jobs">
+				<a :href="job.url">{{ job.title }}</a> - {{ job.company }}
+			</li>
+		</ul>
 	</div>
 </template>
+
+<style scoped lang="scss">
+li {
+	color: #ccc;
+	font-size: 1.3rem;
+	a {
+		color: rgb(218, 218, 128);
+	}
+}
+</style>
