562fbd75af6ba1f58059ed0a7ba6c6b65ccc420c
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-03 15:03:44 +0100

    sass works

--- src/App.vue
+++ src/App.vue
@@ -12,24 +12,23 @@ import { RouterLink, RouterView } from 'vue-router';
 	<RouterView />
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 nav {
 	display: flex;
 	gap: 0.5rem;
 	background-color: #222;
 	padding: 0.5rem 1rem;
-}
-a {
-	color: #ccc;
-	text-decoration: none;
-	font-size: 1.3rem;
+	a {
+		color: #ccc;
+		text-decoration: none;
+		font-size: 1.3rem;
+	}
+	a.router-link-active {
+		color: rgb(207, 207, 110);
+	}
 }
 
 h1 {
 	color: rgb(206, 163, 83);
-}
-
-a.router-link-active {
-	color: rgb(207, 207, 110);
 }
 </style>
