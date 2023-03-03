add12557d9d8e88d206fcafbe4bb2cd615c02caa
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-03 16:09:56 +0100

    skills in

--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+import type { ISkill } from '@/interfaces';
+import axios from 'axios';
+const skills: ISkill[] = (await axios.get('https://edwardtanguay.vercel.app/share/skills.json')).data;
+</script>
+
 <template>
 	<div class="skills">
-		<p>This is the skills page.</p>
+		<p>There are {{ skills.length }} skills</p>
+		<ul>
+			<li v-for="skill in skills">
+				<a :href="skill.url">{{ skill.name }}</a> - {{ skill.description }}
+			</li>
+		</ul>
 	</div>
 </template>
+
+<style scoped lang="scss">
+li {
+	color: #ccc;
+	font-size: 1.3rem;
+	a {
+		color: rgb(218, 218, 128);
+	}
+}
+</style>
