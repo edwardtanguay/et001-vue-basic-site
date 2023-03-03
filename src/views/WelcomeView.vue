593ee607cec73b3b993246f254cbdf228e92686d
Edward Tanguay (windows) <edward@tanguay.info>
2023-03-03 15:36:33 +0100

    welcome page finished

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const showButton = ref(true);
 const greeting = 'Welcome to this site';
-const message = 'This site contains information about available jobs.';
+const message = ref('This site contains information about available jobs.');
+
+const changeMessage = () => {
+	message.value = 'This site also contains skills you need for IT jobs.';
+	showButton.value = false;
+};
 </script>
 
 <template>
 	<div class="home">
 		<h2>{{ greeting }}</h2>
 		<p>{{ message }}</p>
+		<button v-if="showButton" @click="changeMessage">more info</button>
 	</div>
 </template>
 
